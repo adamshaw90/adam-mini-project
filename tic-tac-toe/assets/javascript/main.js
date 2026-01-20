aed00ffab9bf21cce31b4538fa802998de26cac7
“adamshaw90” <adamoshaw90@gmail.com>
2026-01-20 09:35:12 +0000

    Add a checkwinner function to main.js

--- tic-tac-toe/assets/javascript/main.js
+++ tic-tac-toe/assets/javascript/main.js
@@ -13,6 +13,8 @@ boxes.forEach((box) => {
   });
 });
 
+// This stops the game if someone wins
+let gameOver = false;
 
 // Win conditions logic
 
@@ -26,3 +28,24 @@ const winPatterns = [
   [0, 4, 8],
   [2, 4, 6],
 ];
+
+// Check for a winner
+
+function checkWinner() {
+  for (let i = 0; i < winPatterns.length; i++) { // Loop through the winpatterns
+    let a = winPatterns[i][0];
+    let b = winPatterns[i][1]; // The 3 positions in one winline
+    let c = winPatterns[i][2];
+
+    let value1 = boxes[a].textContent;
+    let value2 = boxes[b].textContent; // These 3 read the textcontent to see if there is an x or o
+    let value3 = boxes[c].textContent;
+
+    if (value1 !== "" && value1 === value2 && value1 === value3) {
+      return value1;
+    }
+  }
+  return null;
+}
+
+

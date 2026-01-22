5e155b24f109eb81ffa648ae0171ebd71c1f76b0
“adamshaw90” <adamoshaw90@gmail.com>
2026-01-22 09:02:30 +0000

    Add check winner function, add comments to each line to expain what is going on

--- tic-tac-toe/assets/javascript/main.js
+++ tic-tac-toe/assets/javascript/main.js
@@ -1,23 +1,42 @@
+// This pulls the 9 box elements from the html
 const boxes = document.querySelectorAll(".box");
 
-// Logic for players to click in the boxes and display x or o
 
+// This pulls the winner message ID from the html - a p element that will display the winner
+const winnerMessage = document.querySelector("#winnerMessage");
+
+
+// This stops the game if someone wins - basically it is not game over, until someone wins
+let gameOver = false;
+
+
+// Defining current player as X, so the game always starts with X
 let currentPlayer = "X";
 
-boxes.forEach((box) => {
-  box.addEventListener("click", () => {
-    if (box.textContent !== "") return;
 
-    box.textContent = currentPlayer;
-    currentPlayer = currentPlayer === "X" ? "O" : "X";
+// This is a foreach loop, it is going to listen for a click inside each box and execute the code
+boxes.forEach((box) => {
+  box.addEventListener("click", () => { // Listening for the click
+    if (gameOver) return; // Kill the loop of someone has won
+    
+    if (box.textContent !== "") return; // Stop boxes with content from being overwritten
+    box.textContent = currentPlayer; // Inserts the x or o
+
+    let winner = checkWinner(); // Call the check winner function below
+
+    if (winner !== null) { // If winner does not equal null - someone won the game
+    winnerMessage.textContent = winner + " wins!"; // Display the winner text in winnermessage element
+    gameOver = true; // Stop clicks from working
+    return; // Kill the click event handler
+
+}  
+    currentPlayer = currentPlayer === "X" ? "O" : "X"; // Switches player
   });
 });
 
-// This stops the game if someone wins
-let gameOver = false;
 
-// Win conditions logic
 
+// Defining the winning patterns, each array element is a line in how someone can win
 const winPatterns = [
   [0, 1, 2],
   [3, 4, 5],
@@ -29,18 +48,19 @@ const winPatterns = [
   [2, 4, 6],
 ];
 
-// Check for a winner
+// Check for a winner logic
 
 function checkWinner() {
-  for (let i = 0; i < winPatterns.length; i++) { // Loop through the winpatterns
+  for (let i = 0; i < winPatterns.length; i++) { // Loop through the winpatterns array
     let a = winPatterns[i][0];
-    let b = winPatterns[i][1]; // The 3 positions in one winline
+    let b = winPatterns[i][1]; // The 3 positions that will make up a winline
     let c = winPatterns[i][2];
 
     let value1 = boxes[a].textContent;
-    let value2 = boxes[b].textContent; // These 3 read the textcontent to see if there is an x or o
+    let value2 = boxes[b].textContent; // These 3 read the textcontent to see if there is an x or o inside
     let value3 = boxes[c].textContent;
 
+     // This basically means if value is not empty and matches values 2 and 3, then a winner is detected, if not, the loop returns null meaning no winner found yet.
     if (value1 !== "" && value1 === value2 && value1 === value3) {
       return value1;
     }
